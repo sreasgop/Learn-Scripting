41d54af960f3f84a19c84881a499b96995b98c09
sreasgop <sreasgop@gmail.com>
2024-08-30 18:37:19 +0530

    07_Assets.sh and 07_SourcingFile.sh added

---
+++ 07_Assets.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+name="Chandra Sreas Gop"
+age=19
+profession=" Programmer | Educator | Aspiring Computer Scientist"
+hobby="Reading"
---
+++ 07_SourcingFile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source 07_Assets.sh       # This is the same as importing a library or framework in java or python using import keyword.
+
+# Now we will get to use the variables available within the 07_Assets.sh file. 
+
+echo "$name is $age years old."
+echo "He is a $profession."
+echo "$hobby is his hobby."
+
