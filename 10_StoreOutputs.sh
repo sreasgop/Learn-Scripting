4204c871da694ea31c5b4ff24ca6df92ef494fe3
sreasgop <sreasgop@gmail.com>
2024-09-02 15:57:25 +0530

    10_StoreOutputs.sh added

---
+++ 10_StoreOutputs.sh
@@ -0,0 +1,13 @@
+# In order to be able to store outputs of commands inside a variable in a shell script we can use these two following syntax:
+
+# var=$(command)
+
+# var=`command`
+
+
+user=`whoami`
+lsoutput=$(pwd)
+
+
+echo "User: $user"
+echo "Working Directory: $lsoutput"
