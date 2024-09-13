ebb3a5b56768d5e15a0c98c940ceccd5d73d94a8
sreasgop <sreasgop@gmail.com>
2024-09-13 14:44:13 +0530

    06_TakeInput.sh modified: New methods of taking input using the read command added

--- Fundamentals/06_TakeInput.sh
+++ Fundamentals/06_TakeInput.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-echo Enter your name: 
+# Prompting the user to enter their name:
+echo -ne "\nEnter your name: "
 read name                   # The read command allows us to take input in runtime. 
-
 echo Good Day, $name. 
+
+
+
+# Using the read along with the -p flag to prompt the user to enter their age: 
+read -p $'\nEnter your age: ' age
+echo -e "Your age is $age"
+
+
+
+# Using the read command along with the -s flag to prompt the user to enter something silently:
+read -s -p $'\nEnter Password: ' password
+echo -e "\nYour password: $password"
+
+# NOTE: Unlike the echo command we cannot combine flags together in the read command. i.e. we cannot write read -sp, doing that will throw an error.
