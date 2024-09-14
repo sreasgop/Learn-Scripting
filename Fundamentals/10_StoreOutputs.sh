840a242ffc5f939e347e0a508ec26f21ab3461ca
sreasgop <sreasgop@gmail.com>
2024-09-14 21:56:35 +0530

    10_StoreOutputs.sh modified: Comments added to better explain the concepts covered in this file.

--- Fundamentals/10_StoreOutputs.sh
+++ Fundamentals/10_StoreOutputs.sh
@@ -1,7 +1,7 @@
-# In order to be able to store outputs of commands inside a variable in a shell script we can use these two following syntax:
+#!/bin/bash
 
+# NOTE: In order to be able to store outputs of commands inside a variable in a shell script we can use these two following syntax:
 # var=$(command)
-
 # var=`command`
 
 
