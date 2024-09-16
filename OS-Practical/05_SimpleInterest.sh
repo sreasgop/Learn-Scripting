aedcb9cec13c91ad547fa5d53df499778e08cde3
sreasgop <sreasgop@gmail.com>
2024-09-17 04:47:22 +0530

    05_SimpleInterest.sh modified: A much simpler method to calculate the simple interested implemented

--- OS-Practical/05_SimpleInterest.sh
+++ OS-Practical/05_SimpleInterest.sh
@@ -6,5 +6,5 @@ read -p $'\nEnter Principle Amount: ' amt
 read -p 'Enter Rate of Interest: ' rate
 read -p 'Enter Time in years: ' time
 
-simpleInterest=`echo "scale=2; $((amt * rate * time)) / 100 "| bc`
+simpleInterest=`echo "scale=2; $amt * $rate * $time / 100 "| bc`
 echo -e "\nSimple Interest: $simpleInterest"
