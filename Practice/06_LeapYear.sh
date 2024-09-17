501443d932c83e3df7f191f7cd1bbe6494a9408c
sreasgop <sreasgop@gmail.com>
2024-09-17 17:33:53 +0530

    06_LeapYear.sh modified: All methods of performing the arithmetic calculation to figure out whether a year is leap year or not included.

--- Practice/06_LeapYear.sh
+++ Practice/06_LeapYear.sh
@@ -2,12 +2,29 @@
 
 read -p $'\nEnter year: ' year
 
-if (( year%4==0 && year%100!=0 || year%400==0))
+# if (( year%4==0 && year%100!=0 || year%400==0))
+# then
+#     echo "Leap Year."
+# else
+#     echo "Not a leap year."
+# fi
+
+
+# Another way to calculate leap year: 
+# if [ `expr $year % 4` -eq 0 ] && [ `expr $year % 100` -ne 0 ] || [ `expr $year % 400` -eq 0 ]
+# then
+#     echo "Entered year is a leap year."
+# else
+#     echo "Entered year isn't a leap year."
+# fi
+
+
+# Another way to calculate leap year: 
+if [ `expr $year % 4` -eq 0 -a `expr $year % 100` -ne 0 -o `expr $year % 400` -eq 0 ]
 then
-    echo "Leap Year."
+    echo "LEAP YEAR."
 else
-    echo "Not a leap year."
+    echo "NOT A LEAP YEAR."
 fi
 
 exit
-
