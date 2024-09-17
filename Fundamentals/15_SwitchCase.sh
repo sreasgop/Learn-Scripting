6e61b066cd228860136e7be25897158f7873fb5a
sreasgop <sreasgop@gmail.com>
2024-09-18 05:12:17 +0530

    15_SwitchCase.sh added

---
+++ Fundamentals/15_SwitchCase.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+read -p $'\nEnter number (1-7): ' num
+
+case $num in
+    1)
+        echo "Monday"
+        ;;
+    2)
+        echo "Tuesday"
+        ;;
+    3)
+        echo "Wednesday"
+        ;;
+    4)
+        echo "Thursday"
+        ;;
+    5)
+        echo "Friday"
+        ;;
+    6)
+        echo "Saturday"
+        ;;
+    7)
+        echo "Sunday"
+        ;;
+    *)
+        echo "Invalid Input!"
+        ;;
+esac
+
