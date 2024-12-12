6556ff4458f834825856deaccfb3fca1e15cf285
sreasgop <sreasgop@gmail.com>
2024-12-13 00:31:14 +0530

    13_Armstrong.sh improved.

--- OS-Practical/13_Armstrong.sh
+++ OS-Practical/13_Armstrong.sh
@@ -5,20 +5,12 @@
 read -p $'\nEnter a number: ' num
 
 numcpy=$num
-counter=0
 newnum=0
+let length=${#num}
 
 while((num!=0))
 do
-    ((counter++))
-    ((num/=10))
-done
-
-num=$numcpy
-
-while((num!=0))
-do
-    let "newnum+= (num%10) ** counter"
+    let "newnum+= (num%10) ** length"
     ((num/=10))
 done
 
